9c8e9194787274d9dc6a5de02f14909e7e898422
shakiblimon <shakibcselimon@gmail.com>
2017-04-14 23:45:00 +0600

    'Finding pattern of text without regular expression '

--- re.py
+++ re.py
@@ -7,3 +7,15 @@ def isPhoneNumber (text):
     if text[3] != '-':
         return False
     for i in range (4,7):
+        if not text[i].isdecimal():
+            return False
+    if text[7] != '-':
+        return  False
+    for i in range(8,12):
+        if not text[i].isdecimal():
+            return False
+    return True
+print('415-555-4242 is a phone number: ')
+print(isPhoneNumber('415-555-4242'))
+print('Moshi moshi ia phone number:')
+print(isPhoneNumber('Moshi moshi'))
